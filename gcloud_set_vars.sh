3884c3bef462f0447ff0222c9378da8f311bfa4f
Umer Riaz Kayani <umer.kayani@rewe-digital.com>
2025-07-01 11:22:33 +0200

    Small bugfix.

--- gcloud_set_vars.sh
+++ gcloud_set_vars.sh
@@ -4,5 +4,4 @@ export PYTHONPATH="${PYTHONPATH}:/home/umer_kayani/easy-hr"
 export GCP_PROJECT='rd-team-values-sbx'
 export AR_REPO='streamlit-simple-hr-repo'
 export GCP_REGION='europe-west1'
-export GCP_PROJECT='simple-hr'
 export SERVICE_NAME='simple-hr'
